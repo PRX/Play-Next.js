a839f98930986c50c740768969b6b94e01c681b9
Rick Peterman <rick.peterman@prx.org>
2022-04-14 12:04:03 -0500

    config(lint): add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'next/core-web-vitals',
+    'airbnb',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx', '.svg'] }
+    ],
+    'react/function-component-definition': 0,
+    'react/jsx-props-no-spreading': 0
+  }
+};
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/**
+ * @file prettier.config.js
+ * Configures prettier.
+ */
+
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none'
+};
