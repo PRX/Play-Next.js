d78bd06a99940250912690781da54a8510e8e2b9
a-maci29 <alexa.macias@prx.org>
2022-05-13 16:51:18 -0400

    feat(Embed): Set up layout styles
    - added output for cover art and playlist.
    
    Co-authored-by: Rick Peterman <rpeterman@pri.org>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '@styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '@styles/globals.scss';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="//media.prx.org/favicon-16x16.png"
+          sizes="16x16"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="//media.prx.org/favicon-32x32.png"
+          sizes="32x32"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
