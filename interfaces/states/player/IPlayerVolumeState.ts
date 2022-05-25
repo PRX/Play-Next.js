daed72492cdbeb93b867ef6c07717e226dc6e5d0
Rick Peterman <rick.peterman@prx.org>
2022-05-25 10:11:12 -0500

    fix(iplayervolume): remove unused props

--- interfaces/states/player/IPlayerVolumeState.ts
+++ interfaces/states/player/IPlayerVolumeState.ts
@@ -13,14 +13,4 @@ export interface IPlayerVolumeState {
    * Boolean to mute player.
    */
   muted: boolean;
-
-  /**
-   * Duration of the track in seconds.
-   */
-  duration: number;
-
-  /**
-   * Position user wants to change playback to as a value between 0 and 1.
-   */
-  seeking: number;
 }
