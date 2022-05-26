6983d898aef9395ae2f0ea068333be09551c32b3
Rick Peterman <rick.peterman@prx.org>
2022-05-25 21:13:15 -0500

    feat(theme): add theme vars
    - wire up buttons to theme vars
    - wire up embed placeholders to theme vars
    - add option to provide custom accent color
    - update icon svgs from Google Fonts > Material Icons

--- components/Player/PlayButton/PlayButton.tsx
+++ components/Player/PlayButton/PlayButton.tsx
@@ -4,15 +4,17 @@
  */
 
 import React, { useContext } from 'react';
+import clsx from 'clsx';
 import PlayerContext from '@contexts/PlayerContext';
 import { PlayerActionTypes } from '@states/player/Player.actions';
 import PlayArrowIcon from '@svg/icons/PlayArrow.svg';
 import PauseIcon from '@svg/icons/Pause.svg';
+import styles from './PlayButton.module.scss';
 
 export interface IPlayButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const PlayButton: React.FC<IPlayButtonProps> = ({ ...props }) => {
+const PlayButton: React.FC<IPlayButtonProps> = ({ className, ...props }) => {
   const { state, dispatch } = useContext(PlayerContext);
   const { playing } = state;
 
@@ -21,7 +23,12 @@ const PlayButton: React.FC<IPlayButtonProps> = ({ ...props }) => {
   };
 
   return (
-    <button {...props} type="button" onClick={handleClick}>
+    <button
+      className={clsx(styles.root, styles.iconButton, className)}
+      {...props}
+      type="button"
+      onClick={handleClick}
+    >
       {!playing ? <PlayArrowIcon /> : <PauseIcon />}
     </button>
   );
