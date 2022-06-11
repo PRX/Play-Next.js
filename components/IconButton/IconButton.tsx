a75582244e79c78c5ba39b87cb87fdb852c2f860
Rick Peterman <rick.peterman@prx.org>
2022-06-10 23:41:07 -0500

    fix(menubutton): remove prompt on blur

--- components/IconButton/IconButton.tsx
+++ components/IconButton/IconButton.tsx
@@ -4,6 +4,7 @@
  */
 
 import type React from 'react';
+import { forwardRef } from 'react';
 import clsx from 'clsx';
 import ThemeVars from '@components/ThemeVars';
 import styles from './IconButton.module.scss';
@@ -13,28 +14,31 @@ export interface IIconButtonProps
   href?: string;
 }
 
-const IconButton = ({ children, className = null, href = null, ...props }) => (
-  <>
-    <ThemeVars theme="IconButton" cssProps={styles} />
-    {href ? (
-      <a
-        className={clsx(styles.iconButton, className)}
-        href={href}
-        rel="noreferrer"
-        target="_blank"
-      >
-        {children}
-      </a>
-    ) : (
-      <button
-        {...props}
-        className={clsx(styles.iconButton, className)}
-        type="button"
-      >
-        {children}
-      </button>
-    )}
-  </>
+const IconButton = forwardRef<HTMLButtonElement, IIconButtonProps>(
+  ({ children, className = null, href = null, ...props }, ref) => (
+    <>
+      <ThemeVars theme="IconButton" cssProps={styles} />
+      {href ? (
+        <a
+          className={clsx(styles.iconButton, className)}
+          href={href}
+          rel="noreferrer"
+          target="_blank"
+        >
+          {children}
+        </a>
+      ) : (
+        <button
+          {...props}
+          className={clsx(styles.iconButton, className)}
+          type="button"
+          ref={ref}
+        >
+          {children}
+        </button>
+      )}
+    </>
+  )
 );
 
 export default IconButton;
