9fce875cac6708a0bb8f6db1064c799119332b1f
Rick Peterman <rick.peterman@prx.org>
2022-06-14 14:05:32 -0500

    fix(embed): tab focus menus and modals

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -4,7 +4,7 @@
  *
  */
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import ThemeVars from '@components/ThemeVars';
 import IconButton from '@components/IconButton';
 import CloseIcon from '@svg/icons/Close.svg';
@@ -15,22 +15,30 @@ export interface IModalProps extends React.PropsWithChildren<{}> {
 }
 
 const Modal: React.FC<IModalProps> = ({ children, onClose }) => {
+  const rootRef = useRef<HTMLDivElement>();
+
   const handleClick = () => {
     onClose();
   };
 
+  useEffect(() => {
+    rootRef.current.focus();
+    rootRef.current.blur();
+  });
+
   return (
     <div className={styles.root}>
       <ThemeVars theme="Modal" cssProps={styles} />
-      {children}
-
       <IconButton
+        ref={rootRef}
         type="button"
         className={styles.closeButton}
         onClick={handleClick}
       >
         <CloseIcon />
       </IconButton>
+
+      {children}
     </div>
   );
 };
