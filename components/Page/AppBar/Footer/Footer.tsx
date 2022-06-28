b1a57f56acda4b11d6ad80acae3c00493e11688b
a-maci29 <alexa.macias@prx.og>
2022-06-28 12:58:16 -0400

    feat(footer) debugging

---
+++ components/Page/AppBar/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+/**
+ *
+ * @file Footer.tsx
+ * Component for page footer
+ */
+import Styles from './Footer.module.scss';
+
+const Footer = () => {
+  console.log();
+  return (
+    <div className={Styles.footer}>
+      <div className={Styles.footerLinks}>
+        <h3>Company</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
