ad86bb2c4b7f11c3d05434b709d2b88f97a2cd35
Andrew Kuklewicz <andrew@beginsinwonder.com>
2022-09-21 17:04:46 -0400

    Retrieve and parse podcast:value from RSS feeds

--- lib/fetch/rss/fetchRssFeed.ts
+++ lib/fetch/rss/fetchRssFeed.ts
@@ -1,6 +1,20 @@
 import Parser from 'rss-parser';
+import type { IRss } from '@interfaces/data';
+import type { IRssItem } from '@interfaces/data';
+import {
+  IRssPodcastValue,
+  IRssPodcastValueRecipient
+} from '@interfaces/data/IRssPodcast';
 
-const parser: Parser<Parser.Output<Parser.Item>, Parser.Item> = new Parser();
+type CustomFeed = { 'podcast:value': any };
+type CustomItem = { 'podcast:value': any };
+
+const parser: Parser<CustomFeed, CustomItem> = new Parser({
+  customFields: {
+    feed: ['podcast:value'],
+    item: ['podcast:value']
+  }
+});
 
 class RssProxyError extends Error {
   public url: string;
@@ -16,14 +30,51 @@ class RssProxyError extends Error {
 /**
  * Fetch and parse RSS feed URL.
  * @param feedUrl URL to request feed from.
- * @returns Parsed RSS data object.
+ * @returns Promise for parsed IRSS data object.
  */
-const fetchRssFeed = async (feedUrl: string) => {
+const fetchRssFeed = async (feedUrl: string): Promise<IRss> => {
   try {
-    return await parser.parseURL(feedUrl);
+    const feed = await parser.parseURL(feedUrl);
+    return decoratePodcast(feed);
   } catch (err) {
     throw new RssProxyError(err.message, feedUrl);
   }
+};
+
+const decoratePodcast = (feed: Parser.Output<CustomFeed>): IRss => {
+  const feedItems: IRssItem[] = feed.items.map((item) => {
+    const itemVal = extractPodcastValue(item);
+    return {
+      ...item,
+      ...(itemVal && { podcast: { value: itemVal } })
+    } as IRssItem;
+  });
+
+  const feedVal = extractPodcastValue(feed);
+  const rssData: IRss = {
+    ...feed,
+    ...{ items: feedItems },
+    ...(feedVal && { podcast: { value: feedVal } })
+  };
+
+  delete rssData['podcast:value'];
+
+  return rssData;
+};
+
+const extractPodcastValue = (data): IRssPodcastValue => {
+  const recipients: IRssPodcastValueRecipient[] = data['podcast:value']?.[
+    'podcast:valueRecipient'
+  ].map((recipient) => ({
+    ...recipient.$
+  }));
+
+  const podcastValue: IRssPodcastValue = {
+    ...data['podcast:value']?.$,
+    ...{ valueRecipients: recipients }
+  };
+
+  return podcastValue;
 };
 
 export default fetchRssFeed;
