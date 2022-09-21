76c2df09d966010d976c3a69eb44b264731ca8d3
Andrew Kuklewicz <andrew@beginsinwonder.com>
2022-09-21 17:03:11 -0400

    Add types for podcast:value and podcast:valueRecipient tags

---
+++ interfaces/data/IRssPodcast.ts
@@ -0,0 +1,20 @@
+/**
+ * Defines RSS `podcast:*` namespace interfaces and types.
+ */
+
+export interface IRssPodcastValueRecipient {
+  type: string;
+  address: string;
+  split: number;
+  name?: string;
+  customKey?: string;
+  customValue?: string;
+  fee?: boolean;
+}
+
+export interface IRssPodcastValue {
+  type: string;
+  method: string;
+  suggested?: number;
+  valueRecipient: IRssPodcastValueRecipient[];
+}
