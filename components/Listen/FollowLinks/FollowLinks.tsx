462061b6a278bbcb28f124dd08100afcabb5c298
Rick Peterman <rick.peterman@prx.org>
2025-07-17 16:12:28 -0500

    fix(followlinks): improve scrolling UX for mouse users

--- components/Listen/FollowLinks/FollowLinks.tsx
+++ components/Listen/FollowLinks/FollowLinks.tsx
@@ -5,6 +5,7 @@
 
 import type React from 'react';
 import type { IRssPodcastFollowLink } from '@interfaces/data';
+import { useEffect, useRef } from 'react';
 import root from 'react-shadow';
 import clsx from 'clsx';
 import serviceAssetsMap from '@config/ServiceAssetsMap';
@@ -23,6 +24,47 @@ const FollowLinks: React.FC<IFollowLinksProps> = ({
   links,
   ...props
 }) => {
+  const rootRef = useRef<HTMLDivElement>(null);
+  const innerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const currentRootRef = rootRef.current;
+    const currentInnerRef = innerRef.current;
+    const firstItem = currentInnerRef?.children[0];
+    const resolvedInnerRefStyleMap = window?.getComputedStyle(currentInnerRef);
+
+    /**
+     * Handle mouse move even in root element.
+     * Should not fire on touch deices.
+     * @param e Mouse Event
+     */
+    function handleMouseMove(e: MouseEvent) {
+      const { scrollWidth } = currentRootRef;
+      const paddingInlineStart = parseFloat(
+        resolvedInnerRefStyleMap.paddingInlineStart
+      );
+      const paddingInlineEnd = parseFloat(
+        resolvedInnerRefStyleMap.paddingInlineEnd
+      );
+      const { width, left } = currentRootRef.getBoundingClientRect();
+      const { width: firstItemWidth } = firstItem.getBoundingClientRect();
+      const scrollRatio =
+        // Delay scrolling by inline start padding an width of first item.
+        (e.x - left - paddingInlineStart - firstItemWidth) /
+        // Adjust total width by all in line padding and width of first item.
+        (width - paddingInlineStart - paddingInlineEnd - firstItemWidth);
+
+      currentRootRef.scrollLeft =
+        Math.max(0, scrollWidth - width) * scrollRatio;
+    }
+
+    currentRootRef?.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      currentRootRef.removeEventListener('wheel', handleMouseMove);
+    };
+  }, []);
+
   if (!links || links?.length <= 1) return null;
 
   const followLinks = [...links.slice(0, -1)];
@@ -32,8 +74,8 @@ const FollowLinks: React.FC<IFollowLinksProps> = ({
   const badgeLinks = followLinks.splice(0);
 
   return (
-    <div className={clsx(styles.root, className)} {...props}>
-      <div className={styles.inner}>
+    <div className={clsx(styles.root, className)} {...props} ref={rootRef}>
+      <div className={styles.inner} ref={innerRef}>
         {badgeLinks.map(({ service, href, text }) => {
           const { BadgeComponent, label } = serviceAssetsMap.get(service);
           return (
