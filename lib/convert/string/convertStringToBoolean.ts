55ae1cf62d5589b06a49e77e3d5a2d20c5c08004
Andrew Kuklewicz <andrew@beginsinwonder.com>
2025-10-23 17:35:53 -0400

    Add in correct docs for no

--- lib/convert/string/convertStringToBoolean.ts
+++ lib/convert/string/convertStringToBoolean.ts
@@ -1,7 +1,7 @@
 /**
  * Convert string value into boolean.
  * @param str String to convert.
- * @returns '0', 'false', 'null', 'undefined', or '' as false, all else as true.
+ * @returns '0', 'false', 'null', 'no', 'undefined', or '' as false, all else as true.
  */
 const convertStringToBoolean = (str: string): boolean =>
   ['0', 'false', 'no', 'null', 'undefined'].indexOf(str) > -1 ? false : !!str;
